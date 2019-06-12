a4f5b808e343b2f6c6e04eda215ff483b27cb5d2
Nicolay Poymanov <n.poymanov@gmail.com>
2019-06-12 11:04:15 +0300

    HW2: finished implementation of InMemoryUserRepositoryImpl class

--- src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
@@ -1,12 +1,27 @@
 package ru.javawebinar.topjava.model;
 
-public abstract class AbstractNamedEntity extends AbstractBaseEntity {
+import java.util.Objects;
+
+public abstract class AbstractNamedEntity extends AbstractBaseEntity implements Comparable<AbstractNamedEntity> {
 
     protected String name;
 
     protected AbstractNamedEntity(Integer id, String name) {
         super(id);
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractNamedEntity that = (AbstractNamedEntity) o;
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
     public void setName(String name) {
@@ -20,5 +35,9 @@ public abstract class AbstractNamedEntity extends AbstractBaseEntity {
     @Override
     public String toString() {
         return String.format("Entity %s (%s, '%s')", getClass().getName(), id, name);
+    }
+
+    public int compareTo(AbstractNamedEntity o) {
+        return name.compareTo(o.name);
     }
 }
