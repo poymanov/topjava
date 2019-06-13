113756cae26dcfad379ab37ca25df3b2a8b17c14
Nicolay Poymanov <n.poymanov@gmail.com>
2019-06-13 21:45:00 +0300

    HW2: added user comparator to user repository

--- src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
@@ -2,26 +2,13 @@ package ru.javawebinar.topjava.model;
 
 import java.util.Objects;
 
-public abstract class AbstractNamedEntity extends AbstractBaseEntity implements Comparable<AbstractNamedEntity> {
+public abstract class AbstractNamedEntity extends AbstractBaseEntity {
 
     protected String name;
 
     protected AbstractNamedEntity(Integer id, String name) {
         super(id);
         this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AbstractNamedEntity that = (AbstractNamedEntity) o;
-        return name.equals(that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
     }
 
     public void setName(String name) {
@@ -35,9 +22,5 @@ public abstract class AbstractNamedEntity extends AbstractBaseEntity implements 
     @Override
     public String toString() {
         return String.format("Entity %s (%s, '%s')", getClass().getName(), id, name);
-    }
-
-    public int compareTo(AbstractNamedEntity o) {
-        return name.compareTo(o.name);
     }
 }
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepositoryImpl.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.AbstractNamedEntity;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
@@ -45,7 +46,11 @@ public class InMemoryUserRepositoryImpl implements UserRepository {
         log.info("getAll");
 
         List<User> users = new ArrayList<>(repository.values());
-        users.sort(User::compareTo);
+
+        Comparator<User> comparator = Comparator.comparing(AbstractNamedEntity::getName);
+        comparator = comparator.thenComparing(User::getEmail);
+
+        users.sort(comparator);
 
         return users;
     }
