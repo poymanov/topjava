b63b86f1db3d198412523c12bbbca041297fc824
Nicolay Poymanov <n.poymanov@gmail.com>
2019-07-01 20:53:49 +0300

    HW5: added abstract service test

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -12,6 +12,8 @@ public class Profiles {
             POSTGRES_DB = "postgres",
             HSQL_DB = "hsqldb";
 
+    public static final String DB = HSQL_DB;
+
     //  Get DB profile depending of DB driver in classpath
     public static String getActiveDbProfile() {
         try {
